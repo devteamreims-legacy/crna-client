36b90f703c82f18fb5b7e94dad47b91abb30a297
Benjamin Beret <benjamin.beret@gmail.com>
2015-03-27 19:51:56 +0100

    Ajout du controller "secteur"

---
+++ test/spec/controllers/secteur.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: SecteurCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('crnaClientApp'));
+
+  var SecteurCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    SecteurCtrl = $controller('SecteurCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
