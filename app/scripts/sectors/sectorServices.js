79ddc3b4e83d538c4b8d6c1781d70109a9851e8d
Benjamin Beret <benjamin.beret@gmail.com>
2015-03-30 17:06:01 +0200

    Add a global websocket, make sectorServices depends on it

--- app/scripts/sectors/sectorServices.js
+++ app/scripts/sectors/sectorServices.js
@@ -8,13 +8,7 @@
  * Services managing sectors
  */
 
-angular.module('sectorServices', ['btford.socket-io'])
-// Socket.io Factory
-.factory('socketSecteur', ['socketFactory', function (socketFactory) {
-  var myFactory = socketFactory({
-  });
-  return myFactory;
-}])
-.service('MySectors', ['socketSecteur', function(socketSecteur) {
+angular.module('sectorServices', ['mainWebSocket'])
+.service('MySectors', ['socket', function(socketSecteur) {
   this.mySectors = ['UR', 'XR', 'KR', 'HR'];
 }]);
