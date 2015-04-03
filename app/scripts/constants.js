546e1505555d85c97bcc0fd278bdb041f94bcfe3
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-03 20:17:59 +0200

    Add stub xman data

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -9,28 +9,62 @@
 
 angular.module('crnaConstants', [])
 .constant('crnaAtomicSectors', [
-  'UR', 'XR', 'KR', 'YR', 'HR',
-  'UB', 'UN', 'KN', 'HN',
-  'KD',
-  'UF', 'KF',
-  'E', 'UE', 'XE', 'KE', 'HE',
-  'SE', 'UH', 'XH', 'KH', 'HH'
+    'UR', 'XR', 'KR', 'YR', 'HR',
+    'UB', 'UN', 'KN', 'HN',
+    'KD',
+    'UF', 'KF',
+    'E', 'UE', 'XE', 'KE', 'HE',
+    'SE', 'UH', 'XH', 'KH', 'HH'
 ])
 .constant('crnaSectors',[ 
-  { name: 'UXR',    children: ['UR', 'XR'] },
-  { name: 'UXKR',   children: ['UR', 'XR', 'KR'] },
-  { name: 'KHR',    children: ['KR', 'YR', 'HR'] },
-  { name: 'HYR',    children: ['YR', 'HR'] },
-  { name: '5R',     children: ['UXR', 'KHR'] },
-  { name: 'UBN',    children: ['UN', 'UB'] },
-  { name: 'UBKN',   children: ['UBN', 'KN'] },
-  { name: 'KHN',    children: ['KN', 'HN'] },
-  { name: '4N',     children: ['UB', 'UN', 'KN', 'HN'] }
+    { name: 'UXR',    children: ['UR', 'XR'] },
+    { name: 'UXKR',   children: ['UR', 'XR', 'KR'] },
+    { name: 'KHR',    children: ['KR', 'YR', 'HR'] },
+    { name: 'HYR',    children: ['YR', 'HR'] },
+    { name: '5R',     children: ['UXR', 'KHR'] },
+    { name: 'UBN',    children: ['UN', 'UB'] },
+    { name: 'UBKN',   children: ['UBN', 'KN'] },
+    { name: 'KHN',    children: ['KN', 'HN'] },
+    { name: '4N',     children: ['UB', 'UN', 'KN', 'HN'] }
 ])
 .constant('crnaPositions', [
-  '30', '31', '32', '33', // 4R
-  '34', '35', '36', '37', // 4N
-  '20', '21', '22', '23', // KD + 4H + FIR
-  '24', '25', '26', '27', // 4E + 2F
-  '11', '12', '13', '14'  // FIR + KF + Nuit
+    '30', '31', '32', '33', // 4R
+    '34', '35', '36', '37', // 4N
+    '20', '21', '22', '23', // KD + 4H + FIR
+    '24', '25', '26', '27', // 4E + 2F
+    '11', '12', '13', '14'  // FIR + KF + Nuit
+])
+.constant('xmanStubData', [ // Stub data for xman module
+    // http://google-styleguide.googlecode.com/svn/trunk/jsoncstyleguide.xml?showone=Property_Name_Format#Property_Name_Format
+  {
+    flightId: 1234,
+    callSign: 'BAW63G',
+    delay: 19,
+    speed: -3,
+    applied: {
+      position: 12,
+      when: Date.now - 1000*60*10, // 10 minutes ago
+      sectors: ['UR', 'XR'],
+      speed: -2
+    }
+  },
+  {
+    flightId: 1235,
+    callSign: 'BAW677',
+    delay: 21,
+    speed: -4,
+    applied: { }
+  },
+  { 
+    flightId: 1236,
+    callSign: 'EZS1023',
+    delay: 10,
+    speed: -4,
+    applied: {
+      position: 14,
+      when: Date.now - 1000*60*4, // 4 minutes ago
+      spectors: ['UF', 'KF', 'KD'],
+      speed: -4
+    }
+  }
 ]);
