f1d897dc562ab00b191dfdc9a34edd29b1b06711
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-03 23:16:55 +0200

    Add smart-table angular module (https://github.com/lorenzofox3/Smart-Table)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-socket-io/socket.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-smart-table/dist/smart-table.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
