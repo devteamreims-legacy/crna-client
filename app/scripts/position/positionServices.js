afd0f7b1f89bde8133f8763e1de3c5991f90acce
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-03 18:57:58 +0200

    Refactor services to factories (cleaner code) Correct a bug in websocket updating and angular two way databinding

--- app/scripts/position/positionServices.js
+++ app/scripts/position/positionServices.js
@@ -12,76 +12,87 @@ angular.module('positionServices', [
   'mainWebSocket', // Our main webSocket
   'ngCookies' // Cookies
 ])
-.service('myPosition', ['socketPosition', 'cookiePosition', 'socket', function(socketPosition, cookiePosition, socket) {
-  var vm = this;
+.factory('myPosition', ['socketPosition', 'cookiePosition', 'socket', '$log', function(socketPosition, cookiePosition, socket, $log) {
+  var myPosition = {};
 
-  vm.setPosition = setPosition; // Sets our position, sets a cookie with it, sends update to the remote server
   
-  vm.myPosition = cookiePosition.positionFromCookie();
-  
+  // Load our position from cookie when instantiated
+  myPosition.name = cookiePosition.positionFromCookie().name;
+
   /**
    * Bind websocket events
    */
   // Backend wants to assign us a position
   socket.on('position:set', function (data) { 
-    setPosition(data);
+      $log.debug('Backend assigned us a position : ' + data.name);
+      setPosition(data);
   });
+
   // Backend doesn't know who we are, register our position
-  socket.on('position:get', function(data) {
-    if(vm.myPosition.name === undefined || vm.myPosition.name === '') { // We don't have a position, we can't answer the backend yet
+  socket.on('position:get', function() {
+    if(myPosition.name === undefined || myPosition.name === '') { // We don't have a position, we can't answer the backend yet
+      $log.warn('Backend requested our position, we don\'t know it yet');
       return;
     }
     socketPosition.registerPosition(myPosition);
   });
 
-  
-
   function setPosition(data) {
     if(data === undefined || data.name === undefined || data.name === '') {
       return;
     }
-    // Set local variable
-    vm.myPosition = data;
+    // Set local variable, refresh $rootScope
+    // Here, we could do a myPosition = data; but this would recreate a javascript object and not reference the original one
+    // Thus, ending in not updating the view and losing two way syncing
+    myPosition.name = data.name;
     // Register to the backend
     socketPosition.registerPosition(vm.myPosition);
     // Set the cookie
-    cookiePosition.positionToCookie(vm.myPosition);
+    cookiePosition.positionToCookie(myPosition);
   }
+
+  return { // Return our service
+    myPosition: myPosition,
+    setPosition: setPosition
+  };
 }])
 /* Handles position cookie registration and loading */
-.service('cookiePosition', ['$cookieStore', function($cookieStore) {
-  var vm = this;
-
-  vm.positionToCookie = positionToCookie; // Write cookie with position data
-  vm.positionFromCookie = positionFromCookie; // Load position from cookie
-
+.factory('cookiePosition', ['$cookieStore', '$log', function($cookieStore, $log) {
   var cookieStr = 'my-position'; // Our Cookie string
 
-  function positionToCookie(data) {
+  function positionToCookie(data) { // Write cookie with position data
     if(data === undefined || data.name === undefined) { // Sanitize incoming data
       return false;
     }
+    $log.debug('Storing position cookie : ' + data.name);
     return $cookieStore.put(cookieStr, data);
   }
 
-  function positionFromCookie() {
-
+  function positionFromCookie() { // Load position from cookie
+    $log.debug('Getting position cookie');
     return $cookieStore.get(cookieStr); 
   }
+
+  return {
+    positionFromCookie: positionFromCookie,
+    positionToCookie: positionToCookie
+  };
 }])
 /* Handles position websocket communication */
-.service('socketPosition', ['socket', function(socket) {
-  var vm = this;
+.factory('socketPosition', ['socket', '$log', function(socket, $log) {
   
-  vm.requestPosition = requestPosition; // Request our position from the remote server
-  vm.registerPosition = registerPosition; // Register our position to the remote server
-  vm.myPosition = {}; // position data, our parent service (myPosition) will keep this in sync
-
-  function requestPosition() {
+  function requestPosition() { // Request our position from the remote server
+    $log.debug('Requesting position from backend (socket)');
     return socket.emit('position:request');
   }
 
-  function registerPosition(data) {
+  function registerPosition(data) { // Register our position to the remote server
+    $log.debug('Register position ' + data.name + ' to backend');
     return socket.emit('position:register', data);
   }
+  
+  return {
+    requestPosition: requestPosition,
+    registerPosition: registerPosition
+  };
 }]);
