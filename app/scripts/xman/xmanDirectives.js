5dabba9cc88b846d9a1333e94cfc728873e6f4e5
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-05 10:51:04 +0200

    Add 2 directives (appliedBy / apply) to handle xman

--- app/scripts/xman/xmanDirectives.js
+++ app/scripts/xman/xmanDirectives.js
@@ -7,12 +7,34 @@
  * # xmanDirectives
  * Directives for xman
  **/
-angular.module('xmanDirectives', [])
+angular.module('xmanDirectives', ['smart-table'])
+// Xman flight list directive
 .directive('xmanFlightList', function() {
   return {
     restrict: 'EA',
     controller: 'XmanController', // Link to Position Controller
     controllerAs: 'x',
     templateUrl: 'views/xman/_xmanFlightList.html'
+  };
+})
+// Xman applied by snippet
+.directive('xmanAppliedBy', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      flightId: '=',
+      applied: '='
+    },
+    templateUrl: 'views/xman/_xmanAppliedBy.html'
+  };
+})
+.directive('xmanApply', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      flightId: '=',
+      applied: '='
+    },
+    templateUrl: 'views/xman/_xmanApply.html'
   };
 });
