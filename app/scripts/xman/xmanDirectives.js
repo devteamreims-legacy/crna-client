a7732e35ccf26562e9688d23bead98bd326b7f53
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-06 11:59:54 +0200

    Copy current xman behaviour and color scheme

--- app/scripts/xman/xmanDirectives.js
+++ app/scripts/xman/xmanDirectives.js
@@ -38,6 +38,33 @@ angular.module('xmanDirectives', ['crnaConstants', 'smart-table'])
     controllerAs: 'xm',
     controller: function($scope, xmanDefaultSpeeds) {
       $scope.xmanDefaultSpeeds = xmanDefaultSpeeds; // Pass default speeds to our scope
+      /*
+       * Returns a css class given an xmanFlight and a buttonSpeed
+       */
+      $scope.buttonClass = function(flight, buttonSpeed) {
+        var def = '';
+
+        if (flight.applied.speed === undefined) { // Nothing has been applied yet
+          if (buttonSpeed === flight.speed) {
+            def = 'btn-success'; // Requested speed will show as primary
+          }
+        } else { // We have an applied speed reduction
+          if (flight.speed === flight.applied.speed) { // Applied speed equals requested speed
+            if (buttonSpeed === flight.applied.speed) { // All is good, disable input
+              def = 'btn-success disabled';
+            }
+          } else {
+            if (buttonSpeed === flight.applied.speed) {
+              def = 'btn-warning disabled';
+            }
+            if (buttonSpeed === flight.speed) {
+              def = 'btn-success';
+            }
+          }
+        }
+
+        return def;
+      }
     }
   };
 }]);
