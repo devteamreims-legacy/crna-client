c7820772782b637b349112a0d319ad983afd0413
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-11 15:17:40 +0200

    Add meta module sector

---
+++ app/scripts/sectors/sector.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crnaClientApp.sector
+ * @description
+ * # sector module
+ * Meta module to include EAP components
+ */
+angular.module('sector', ['sectorController', 'sectorDirectives', 'sectorFilters', 'sectorServices']);
--- app/scripts/sectors/sectorController.js
+++ app/scripts/sectors/sectorController.js
@@ -7,7 +7,7 @@
  * # sectorController
  * Controller of the crnaClientApp
  */
-angular.module('sector', ['sectorServices', 'sectorFilters', 'sectorDirectives'])
+angular.module('sectorController', ['sectorServices', 'sectorFilters', 'sectorDirectives'])
 .controller('SectorController', ['$scope', 'mySectors', function ($scope, mySectors) {
   var vm = this;
   vm.mySectors = mySectors.mySectors;
