b48efe7cd9d582954411e5938db9a5f30da4dacf
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-14 16:10:01 +0200

    Revamp configuration dialog (use Lumx)

--- app/scripts/controllers/common.js
+++ app/scripts/controllers/common.js
@@ -24,30 +24,4 @@ angular.module('commonControllers', ['positionServices'])
       templateUrl: 'views/help/' + $state.current.name + 'Help.html'
     });
   };
-}])
-.controller('configController', ['$scope', '$window', 'LxDialogService', function($scope, $window, LxDialogService) {
-  $scope.reloadPage= function() {
-    return $window.location.reload();
-  };
-
-  $scope.positionModal = function() {
-    return;
-    return $modal.open({
-      controller: ['$scope', '$modalInstance', 'myPosition', function($scope, $modalInstance, myPosition) {
-        $scope.closePositionModal = function() {
-          $modalInstance.dismiss();
-        };
-        
-        $scope.my = {selectedPosition: angular.copy(myPosition.myPosition.name)}; // Copy our current position
-
-        $scope.savePositionAndClose = function() {
-          console.log('Scope :', $scope.my.selectedPosition);
-          myPosition.setPosition({name: $scope.my.selectedPosition});
-          $modalInstance.dismiss();
-        };
-
-      }],
-      templateUrl: 'views/config/positionModal.html'
-    });
-  };
 }]);
--- app/scripts/navbar/navbarDirectives.js
+++ app/scripts/navbar/navbarDirectives.js
@@ -7,7 +7,7 @@
  * # navbarDirectives
  * Directives for xman
  **/
-angular.module('navbarDirectives', [])
+angular.module('navbarDirectives', ['lumx', 'positionServices'])
 // Reload Application 
 .directive('crnaReloadApp', function() {
   return {
@@ -18,8 +18,48 @@ angular.module('navbarDirectives', [])
         $window.location.reload();
       };
     }],
-    controllerAs: 'c',
+    controllerAs: 'reloadApp',
     transclude: true,
-    template: '<div ng-click="c.reloadApp()" ng-transclude></div>'
+    template: '<div ng-click="reloadApp.reloadApp()" ng-transclude></div>'
+  };
+})
+// Handle dialog linking
+.directive('crnaChoosePosition', function() {
+  return {
+    restrict: 'A',
+    transclude: true,
+    controller: ['LxDialogService',  function(LxDialogService) {
+      var vm = this;
+      vm.openPositionDialog = function() {
+        LxDialogService.open('position-dialog');
+      };
+    }],
+    controllerAs: 'choosePosition',
+    template: '<div ng-click="choosePosition.openPositionDialog()" ng-transclude></div>' +
+              '<div ng-include="\'views/config/positionDialog.html\'"></div>'
+  };
+})
+// What's inside the dialog
+.directive('crnaChoosePositionForm', function() {
+  return {
+    restrict: 'E',
+    controller: ['$scope', 'myPosition', 'LxDialogService', function($scope, myPosition, LxDialogService) {
+      var vm = this;
+      vm.my = {selectedPosition: angular.copy(myPosition.myPosition.name)};
+
+      vm.closeDialog = function() {
+        LxDialogService.close('position-dialog');
+      };
+
+      vm.saveAndCloseDialog = function() {
+        console.log('Saving position:', vm.my.selectedPosition);
+        myPosition.setPosition({name: vm.my.selectedPosition});
+        vm.closeDialog();
+      };
+    }],
+    controllerAs: 'choosePositionForm',
+    templateUrl: 'views/config/_positionChoiceForm.html'
   };
 });
+
+
