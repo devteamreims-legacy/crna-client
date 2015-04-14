36beda2aec9c35b5d880c79421ee2175a577e6f1
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-14 13:32:28 +0200

    Reimport google fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,7 +217,7 @@ module.exports = function (grunt) {
         importPath: './bower_components',
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/fonts',
+        httpFontsPath: '/styles/fonts',
         relativeAssets: false,
         assetCacheBuster: false,
         raw: 'Sass::Script::Number.precision = 10\n'
