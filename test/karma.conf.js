cc90e74048081bb22bb3ff27b1da42dee882e4a3
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-15 16:41:05 +0200

    Remove lumx

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,7 +19,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -34,9 +33,6 @@ module.exports = function(config) {
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
       'bower_components/underscore/underscore.js',
-      'bower_components/velocity/velocity.js',
-      'bower_components/velocity/velocity.ui.js',
-      'bower_components/lumx/dist/lumx.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
