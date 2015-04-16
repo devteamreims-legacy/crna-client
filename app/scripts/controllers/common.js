d91ffc1be5efe6013bd026f968af54985847c9e7
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-16 23:45:46 +0200

    Remove unused controller

--- app/scripts/controllers/common.js
+++ app/scripts/controllers/common.js
@@ -8,20 +8,7 @@
  * # MainCtrl
  * Controller of the crnaClientApp
  */
-angular.module('commonControllers', ['positionServices'])
+angular.module('commonControllers', [])
 .controller('mainController', function () {
-})
-.controller('navBarController', ['$scope', '$state', 'LxDialogService', function($scope, $state, LxDialogService) {
-  $scope.showHelp = function() {
-    return;
-    return $modal.open({
-      size: 'lg',
-      controller: ['$scope', '$modalInstance', function($scope, $modalInstance) {
-        $scope.closeHelp = function() {
-          $modalInstance.dismiss();
-        };
-      }],
-      templateUrl: 'views/help/' + $state.current.name + 'Help.html'
-    });
-  };
-}]);
+});
+
