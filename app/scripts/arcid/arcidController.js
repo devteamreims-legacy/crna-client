6df826080d72847304b2c2142bf058534b329352
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-17 01:39:57 +0200

    Add flight will not add an undefined flight

--- app/scripts/arcid/arcidController.js
+++ app/scripts/arcid/arcidController.js
@@ -14,6 +14,9 @@ angular.module('arcidController', ['crnaConstants', 'arcidDirectives', 'arcidSer
   arcid.displayedFlights = [];
 
   $scope.addDisplayedFlight = function(f) {
+    if(f === undefined) {
+      return;
+    }
     arcid.displayedFlights = _.without(arcid.displayedFlights, f); // Remove if already present
     arcid.displayedFlights.unshift(f);
     arcid.displayedFlights = arcid.displayedFlights.slice(0, 4); // Keep 4 elements only
