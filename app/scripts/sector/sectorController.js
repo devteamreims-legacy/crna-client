0b63876ae7331ed63f15f0a10cf1789b5f951693
Benjamin Beret <benjamin.beret@gmail.com>
2015-04-18 11:43:22 +0200

    Basic traffic load data

--- app/scripts/sector/sectorController.js
+++ app/scripts/sector/sectorController.js
@@ -7,13 +7,14 @@
  * # sectorController
  * Controller of the crnaClientApp
  */
-angular.module('sectorController', ['sectorServices', 'sectorFilters', 'sectorDirectives'])
+angular.module('sectorController', ['sectorServices', 'sectorFilters', 'sectorDirectives', 'trafficLoadServices'])
 .controller('SectorController', ['$scope', 'mySectors', function ($scope, mySectors) {
   var vm = this;
   vm.mySectors = mySectors.mySectors;
 }])
-.controller('MySectorStatusController', ['$scope', 'mySectors', 'myPosition', function($scope, mySectors, myPosition) {
+.controller('MySectorStatusController', ['$scope', 'mySectors', 'myPosition', 'myTrafficLoad', function($scope, mySectors, myPosition, myTrafficLoad) {
   var vm = this;
   vm.mySectors = mySectors.mySectors;
   vm.myPosition = myPosition.myPosition;
+  $scope.myTrafficLoad = new myTrafficLoad().myLoad;
 }]);
