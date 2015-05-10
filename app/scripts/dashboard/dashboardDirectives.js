dac4ef8cbb10e478e4112536cc30ac44db1dd15f
Benjamin Beret <benjamin.beret@gmail.com>
2015-05-10 19:01:22 +0200

    dashboard stuff

---
+++ app/scripts/dashboard/dashboardDirectives.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardDirectives 
+ * @description
+ * # dashboardDirectives 
+ * Directives for the dashboard 
+ **/
+angular.module('dashboardDirectives', ['crnaConstants', 'sectorServices', 'positionServices', 'underscore']);
