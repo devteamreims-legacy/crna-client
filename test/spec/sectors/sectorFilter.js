4d598d2522d771bdfaea90e234ed4d56a3485b82
Benjamin Beret <benjamin.beret@gmail.com>
2015-05-13 11:13:15 +0200

    Refactor sectorFilter tests

--- test/spec/sectors/sectorFilter.js
+++ test/spec/sectors/sectorFilter.js
@@ -5,10 +5,17 @@ describe('Filters: MySectors', function () {
   // load the controller's module
   beforeEach(module('sectorFilters'));
 
-  describe('toString', function() {
-    it('should return an X when no sectors are set',
-      inject(function(toStringFilter) {
-        expect(toStringFilter([])).toBe('X');
-    }));
+  describe('sectorsToString', function() {
+    it('should return an - when no sectors are set',
+      inject(function(sectorsToStringFilter) {
+        expect(sectorsToStringFilter([])).toBe('-');
+      })
+    );
+
+    it('should return UXR when sectors are UR and XR',
+      inject(function(sectorsToStringFilter) {
+        expect(sectorsToStringFilter(['UR', 'XR'])).toBe('UXR');
+      })
+    );
   });
 });
